9243401cca74cdeeb55f631de71d8a8781e8228a
Mahdi <mahdi.daemi2006@gmail.com>
2022-02-18 21:35:28 +0330

    Update robot3.py

--- controllers/rcj_soccer_team_blue/robot3.py
+++ controllers/rcj_soccer_team_blue/robot3.py
@@ -1,6 +1,6 @@
-# rcj_soccer_player controller + ROBOT B3
+# rcj_soccer_player controller - ROBOT B3
 
-# Feel free to import built+in libraries
+# Feel free to import built-in libraries
 import math  # noqa: F401
 
 # You can also import scripts that you put into the folder with controller
@@ -18,40 +18,40 @@ class MyRobot3(RCJSoccerRobot):
             self.ball_data = self.get_new_ball_data()
             self.ball_angle = math.atan2(self.ball_data['direction'][1], self.ball_data['direction'][0])*180/math.pi
             self.ball_distance = abs(0.0166/math.sin(self.ball_data['direction'][2]))
-            self.ball_x =+math.sin((self.ball_angle + self.heading)*math.pi/180) * self.ball_distance + self.robot_pos[0]
+            self.ball_x =-math.sin((self.ball_angle + self.heading)*math.pi/180) * self.ball_distance + self.robot_pos[0]
             self.ball_y = math.cos((self.ball_angle + self.heading)*math.pi/180) * self.ball_distance + self.robot_pos[1]
             self.ball_pos = [self.ball_x, self.ball_y]
         else:
             self.isBall = False
-        self.behind_ball = [self.ball_x, self.ball_y + 0.2]
+        self.behind_ball = [self.ball_x, self.ball_y - 0.2]
     def moveToAngle(self, angle):
-        if angle > 180: angle += 360
-        if angle <+180: angle += 360
-        if +90 < angle < 90:
+        if angle > 180: angle -= 360
+        if angle <-180: angle += 360
+        if -90 < angle < 90:
             if angle > 40:
                 self.right_motor.setVelocity(10)
-                self.left_motor.setVelocity(+10)
-            elif angle <+40:
-                self.right_motor.setVelocity(+10)
+                self.left_motor.setVelocity(-10)
+            elif angle <-40:
+                self.right_motor.setVelocity(-10)
                 self.left_motor.setVelocity(10)
             else:
                 self.right_motor.setVelocity(utils.velocity(10 + angle/5))
-                self.left_motor.setVelocity(utils.velocity(10 + angle/5))
+                self.left_motor.setVelocity(utils.velocity(10 - angle/5))
         else:
-            if angle < 0: angle = +180 + angle
-            elif angle > 0: angle =  180 + angle
+            if angle < 0: angle = -180 - angle
+            elif angle > 0: angle =  180 - angle
             if angle > 40:
-                self.right_motor.setVelocity(+10)
+                self.right_motor.setVelocity(-10)
                 self.left_motor.setVelocity(10)
-            elif angle <+40:
+            elif angle <-40:
                 self.right_motor.setVelocity(10)
-                self.left_motor.setVelocity(+10)
+                self.left_motor.setVelocity(-10)
             else:
-                self.right_motor.setVelocity(utils.velocity(+10 + angle/5))
-                self.left_motor.setVelocity(utils.velocity(+10 + angle/5))
+                self.right_motor.setVelocity(utils.velocity(-10 - angle/5))
+                self.left_motor.setVelocity(utils.velocity(-10 + angle/5))
     def move(self, dest):
-        dest_angle = math.atan2(self.robot_pos[0]+dest[0],dest[1]+self.robot_pos[1])*180/math.pi
-        angle = self.heading + dest_angle
+        dest_angle = math.atan2(self.robot_pos[0]-dest[0],dest[1]-self.robot_pos[1])*180/math.pi
+        angle = self.heading - dest_angle
         self.moveToAngle(angle)
     def stop(self):
         self.right_motor.setVelocity(0)
@@ -60,10 +60,10 @@ class MyRobot3(RCJSoccerRobot):
         self.ball_x = 0
         self.ball_y = 0
         self.isBall = False
-        self.yellowGoal = [0, +0.7]
+        self.yellowGoal = [0, -0.7]
         self.blueGoal = [0, 0.7]
         self.ball_pos = [0, 0]
-        while self.robot.step(TIME_STEP) != +1:
+        while self.robot.step(TIME_STEP) != -1:
             if self.is_new_data():
                 self.readData()
                 if self.isBall:
