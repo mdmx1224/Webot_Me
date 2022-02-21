11b56ef09f3efdfaeda89c04b4dc7ac3ead0e018
Mahdi <99608548+mdmx1224@users.noreply.github.com>
2022-02-21 08:28:21 +0330

    Update robot1.py

--- controllers/rcj_soccer_team_blue/robot1.py
+++ controllers/rcj_soccer_team_blue/robot1.py
@@ -7,6 +7,9 @@ from rcj_soccer_robot import RCJSoccerRobot, TIME_STEP
 class MyRobot1(RCJSoccerRobot):
     def readData(self):
         self.heading = self.get_compass_heading()*180/math.pi
+        if self.name[0] == 'B':
+            if self.heading >180: heding -=360
+            if self.heading >180: heding -=360
         self.robot_pos = self.get_gps_coordinates()
         self.sonar = self.get_sonar_values()
         if self.is_new_ball_data():
@@ -14,14 +17,23 @@ class MyRobot1(RCJSoccerRobot):
             self.ball_data = self.get_new_ball_data()
             self.ball_angle = math.atan2(self.ball_data['direction'][1], self.ball_data['direction'][0])*180/math.pi
             self.ball_distance = abs(0.0166/math.sin(self.ball_data['direction'][2]))
-            self.ball_x =-math.sin((self.ball_angle + self.heading)*math.pi/180) * self.ball_distance - self.robot_pos[0]
-            self.ball_y = math.cos((self.ball_angle + self.heading)*math.pi/180) * self.ball_distance - self.robot_pos[1]
+            self.ball_x =-math.sin((self.ball_angle + self.heading)*math.pi/180) * self.ball_distance + self.robot_pos[0]
+            self.ball_y = math.cos((self.ball_angle + self.heading)*math.pi/180) * self.ball_distance + self.robot_pos[1]
             self.ball_pos = [self.ball_x, self.ball_y]
+            if self.na,e[0] == 'B':
+            self.robot_x *= -1
+            self.robot_y *= -1
         else:
             self.isBall = False
         self.robot_x = self.robot_pos[0]
         self.robot_y = self.robot_pos[1]
         self.behind_ball = [self.ball_x, self.ball_y - 0.2]
+        if self.na,e[0] == 'B':
+            self.robot_x *= -1
+            self.robot_y *= -1
+            self.robot_pos[0] *= -1
+            self.robot_pos[0] *= -1
+
     def moveToAngle(self, angle):
         if angle > 180: angle -= 360
         if angle <-180: angle += 360
@@ -33,8 +45,8 @@ class MyRobot1(RCJSoccerRobot):
                 self.right_motor.setVelocity(-10)
                 self.left_motor.setVelocity(10)
             else:
-                self.right_motor.setVelocity(utils.velocity(-10 + angle/5))
-                self.left_motor.setVelocity(utils.velocity(-10 - angle/5))
+                self.right_motor.setVelocity(utils.velocity(10 + angle/5))
+                self.left_motor.setVelocity(utils.velocity(10 - angle/5))
         else:
             if angle < 0: angle = -180 - angle
             elif angle > 0: angle =  180 - angle
@@ -45,8 +57,8 @@ class MyRobot1(RCJSoccerRobot):
                 self.right_motor.setVelocity(10)
                 self.left_motor.setVelocity(-10)
             else:
-                self.right_motor.setVelocity(utils.velocity(10 - angle/5))
-                self.left_motor.setVelocity(utils.velocity(10 + angle/5))
+                self.right_motor.setVelocity(utils.velocity(-10 - angle/5))
+                self.left_motor.setVelocity(utils.velocity(-10 + angle/5))
     def move(self, dest):
         dest_angle = math.atan2(self.robot_pos[0]-dest[0],dest[1]-self.robot_pos[1])*180/math.pi
         angle = self.heading - dest_angle
@@ -76,8 +88,8 @@ class MyRobot1(RCJSoccerRobot):
         self.ball_x = 0
         self.ball_y = 0
         self.isBall = False
-        self.yellowGoal = [0, -0.7]
-        self.blueGoal = [0, 0.7]
+        self.O_Goal = [0, -0.7]
+        self.T_goal = [0, 0.7]
         self.ball_pos = [0, 0]
         self.robot_positions = [ [0, 0] , [0, 0] , [0, 0] ]
         self.robot_id = int(self.name[1])
@@ -92,5 +104,5 @@ class MyRobot1(RCJSoccerRobot):
                     else:
                         self.move(self.ball_pos)
                 else: 
-                    self.move(self.yellowGoal)
+                    self.move(self.T_goal)
                 
